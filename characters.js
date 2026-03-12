a9a95c247eb3d70c7b1d9fbaa65adf247c3692df
CloverBClover <robindrawnbydesign@gmail.com>
2026-03-12 19:42:52 +0000

    Update characters.js

--- characters.js
+++ characters.js
@@ -1,47 +1,110 @@
-const characters = [
+const characters=[
 
 {
 name:"KRIS",
-lightner:true,
+lightner:"Lightner",
 chapter:1,
-role:["hero"],
-gender:"enby",
+role:["Party"],
+gender:"Enby",
 undertale:false
 },
 
 {
 name:"SUSIE",
-lightner:true,
+lightner:"Lightner",
 chapter:1,
-role:["hero"],
-gender:"female",
+role:["Party"],
+gender:"Female",
 undertale:false
 },
 
 {
 name:"RALSEI",
-lightner:false,
+lightner:"Darkner",
 chapter:1,
-role:["hero","prince"],
-gender:"male",
-undertale:false
-},
-
-{
-name:"LANCER",
-lightner:false,
-chapter:1,
-role:["villain","prince"],
-gender:"male",
+role:["Party","Prince"],
+gender:"Male",
 undertale:false
 },
 
 {
 name:"NOELLE",
-lightner:true,
+lightner:"Lightner",
 chapter:2,
-role:["hero"],
-gender:"female",
+role:["Party"],
+gender:"Female",
+undertale:false
+},
+
+{
+name:"LANCER",
+lightner:"Darkner",
+chapter:1,
+role:["Enemy"],
+gender:"Male",
+undertale:false
+},
+
+{
+name:"SEAM",
+lightner:"Darkner",
+chapter:1,
+role:["Shop Keeper"],
+gender:"Male",
+undertale:false
+},
+
+{
+name:"JEVIL",
+lightner:"Darkner",
+chapter:1,
+role:["Secret Boss"],
+gender:"Male",
+undertale:false
+},
+
+{
+name:"KING",
+lightner:"Darkner",
+chapter:1,
+role:["Boss"],
+gender:"Male",
+undertale:false
+},
+
+{
+name:"BERDLY",
+lightner:"Lightner",
+chapter:2,
+role:["Enemy"],
+gender:"Male",
+undertale:false
+},
+
+{
+name:"QUEEN",
+lightner:"Darkner",
+chapter:2,
+role:["Boss"],
+gender:"Female",
+undertale:false
+},
+
+{
+name:"SPAMTON",
+lightner:"Darkner",
+chapter:2,
+role:["Enemy","Shop Keeper"],
+gender:"Male",
+undertale:false
+},
+
+{
+name:"SPAMTON NEO",
+lightner:"Darkner",
+chapter:2,
+role:["Secret Boss"],
+gender:"Male",
 undertale:false
 }
 
